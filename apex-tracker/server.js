66206a1edee4df23661b9c4545867634742f8c83
Tim Marks <timmarks90@gmail.com>
2019-09-17 15:07:22 -0700

    Added profile route

---
+++ apex-tracker/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// Load Env config
+dotenv.config({
+    path: './config.env'
+});
+
+const app = express();
+
+// Dev logging
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Profile routes
+app.use('/api/v1/profile', require('./src/routes/profile'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
---
+++ apex-tracker/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+import Results from './components/Results';
+
+function App() {
+  return (
+    <div>
+    <Jumbotron>
+      <Container>
+        <h1>Search App</h1>
+        <p>This is a simple search app</p>
+        <SearchForm />
+      </Container>
+    </Jumbotron>
+    <Results />
+    </div>
+  );
+}
+
+export default App;
