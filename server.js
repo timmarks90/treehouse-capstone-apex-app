0948fd27323d39460dbf30001026a2d7cc21532c
Tim Marks <timmarks90@gmail.com>
2019-10-13 11:00:00 -0700

    initial deployment

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+// Single Page App
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+});
+
 // Profile routes
 app.use('/api/v1/profile', require('./routes/profile'));
 
